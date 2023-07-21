c058c7118cd5a004de90f822c9308e89b13fef65
Szymon Harat <Szymon.Harat@equiniti.com>
2023-07-21 14:54:26 +0200

    Mainfest changes - only message all left and icon

--- samples/bot-conversation/csharp/Bots/TeamsConversationBot.cs
+++ samples/bot-conversation/csharp/Bots/TeamsConversationBot.cs
@@ -177,7 +177,7 @@ namespace Microsoft.BotBuilderSamples.Bots
 
             foreach (var teamMember in members)
             {
-                var proactiveMessage = MessageFactory.Text($"Hello {teamMember.GivenName} {teamMember.Surname}. I'm Architecure Community bot. {TextToShow.Substring(17)}");
+                var proactiveMessage = MessageFactory.Text($"Hello {teamMember.GivenName} {teamMember.Surname}. I'm Architecure Community Bot. {TextToShow.Substring(17)}");
 
                 var conversationParameters = new ConversationParameters
                 {
